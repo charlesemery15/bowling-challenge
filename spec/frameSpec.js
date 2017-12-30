db7f3d862d462e282a72ed4d61db6a3646efdeac
charles <charlesemery15@gmail.com>
2017-12-30 11:18:10 +0000

    Can record the number of bowls in a frame

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -8,4 +8,9 @@ describe('Frame', function (){
   it('should start with a 0 count', function(){
     expect(frame.bowlCount.length).toEqual(0);
   });
+
+  it('should allow a player to record a bowl', function(){
+    frame.bowl(7)
+    expect(frame.bowlCount.length).toEqual(1);
+  });
 });
--- src/frame.js
+++ src/frame.js
@@ -1,3 +1,8 @@
 function Frame(){
   this.bowlCount = []
 };
+
+Frame.prototype.bowl = function(){
+  this.bowlCount += 1
+  return this.bowlCount.length;
+};
